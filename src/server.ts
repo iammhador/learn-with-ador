b771147e4a38f6e771555e39bf566c28d39ba36f
mehedy3107 <mhador111@gmail.com>
2023-05-29 19:34:58 +0600

    [ DONE ] Setup mongoose, express, cors, perser

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+    console.log(`🛢Database connected successfully`);
+  } catch (error) {
+    console.log(`⚠ Failed to connect database`, error);
+  }
+}
+
+main().catch((err) => console.log(err));
